a1c52e35c51e9fa270de8c4eca4115e8d2e0292b
jasmo2 <jasmo2.com@gmail.com>
2016-01-16 13:27:45 -0500

    Add struck to keep weather data in the session

---
+++ wheaterSwift2/WeatherStruct.swift
@@ -0,0 +1,24 @@
+//
+//  WeatherStruct.swift
+//  wheaterSwift2
+//
+//  Created by Jaime Molina Orbe on 1/16/16.
+//  Copyright © 2016 jasmo2. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherStruct {
+    let cityName: String
+    let description : String
+    let kelvin: Double
+    
+    init (cityName: String, description: String, kelvin: Double){
+        self.cityName = cityName
+        self.description = description
+        self.kelvin = kelvin
+    }
+    
+    
+    
+}
