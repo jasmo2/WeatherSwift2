6a31b2e6c0ec3060c248e88102830299f2420457
jasmo2 <jasmo2.com@gmail.com>
2016-01-16 13:47:11 -0500

    Connection to external API

--- wheaterSwift2/WeatherClass.swift
+++ wheaterSwift2/WeatherClass.swift
@@ -17,6 +17,16 @@ class WeatherClass {
     
     var delegate: WeatherClassDelegate?
     func getWeather(city: String){
+        
+        let path = "http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=2de143494c0b295cca9337e1e96b00e0"
+        let url = NSURL(string: path)
+        let session = NSURLSession.sharedSession()
+        let task = session.dataTaskWithURL(url!) { (data: NSData?, response: NSURLResponse?, err: NSError?) -> Void in
+            print("Is done processing URL")
+        }
+        task.resume()
+        
+        
         let weather = WeatherStruct(cityName: city, description: "A brief description", kelvin: 293.15)
 
         if delegate != nil{
